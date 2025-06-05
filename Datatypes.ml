2671e7385ef523fe33767af43210b53d8a9e5a40
jspast <140563347+jspast@users.noreply.github.com>
2025-06-04 22:46:32 -0300

    Initial typeInfer version

--- Datatypes.ml
+++ Datatypes.ml
@@ -1,34 +1,84 @@
-type bop =  
+(* operações binárias *)
+type bop =
   | Sum | Sub | Mul | Div   (* operações aritméticas *)
   | Eq  | Neq | Lt | Gt   (* operações relacionais  *)
-  | And | Or   (* operações lógicas *) 
+  | And | Or   (* operações lógicas *)
 
-type tipo = 
+(* tipos *)
+type tipo =
   | TyInt
   | TyBool
   | TyRef of tipo
   | TyUnit
-    
 
-type expr = 
+(* expressões *)
+type expr =
   | Num of int
-  | Bool of bool 
-  | Id of string
-  | If of expr * expr * expr 
-  | Binop of bop * expr * expr
-  | Wh of expr * expr 
-  | Asg of expr * expr 
-  | Let of string * tipo * expr * expr 
+  | Bool of bool
+  | Id of string   (* endereço de memória *)
+  | If of expr * expr * expr
+  | Binop of bop * expr * expr   (* operações binárias *)
+  | Wh of expr * expr   (* while *)
+  | Asg of expr * expr   (* assignment *)
+  | Let of string * tipo * expr * expr
   | New of expr
-  | Deref of expr 
+  | Deref of expr
   | Unit
   | Seq of expr * expr
   | Read
   | Print of expr
-  
-      
 
-          
+(* typeInfer : expr -> tipo option *)
+let rec typeInfer (e:expr) : tipo option =
+  match e with
+  | Num n -> Some TyInt
+  | Bool b -> Some TyBool
+  | Id e' -> None (* TODO: tipo de um endereço *)
+  | If (e1, e2, e3) -> (match typeInfer e1 with
+    | Some TyBool -> (match typeInfer e2, typeInfer e3 with
+      | Some t2, Some t3 -> if t2=t3
+        then Some t2 else None
+      | _ -> None)
+    | _  -> None)
+  | Binop (op, e1, e2) -> (match op with
+    | Sum | Sub | Mul | Div -> (match typeInfer e1, typeInfer e2 with
+      | Some TyInt, Some TyInt -> Some TyInt
+      | _ -> None)
+    | Eq  | Neq | Lt | Gt | And | Or -> (match typeInfer e1, typeInfer e2 with
+      | Some TyBool, Some TyBool -> Some TyBool
+      | _ -> None))
+  | Wh (e1, e2) -> (match typeInfer e1, typeInfer e2 with
+    | Some TyBool, Some TyUnit -> Some TyUnit
+    | _ -> None)
+  | Asg (e1, e2) -> (match typeInfer e1, typeInfer e2 with
+    | Some TyRef t1, Some t2 -> if t1=t2
+      then Some TyUnit else None
+    | _ -> None)
+  | Let (x, t, e1, e2) -> (match t, typeInfer e1, typeInfer e2 with
+    | t, Some t1, Some t2 -> if t1=t
+      then Some t2 else None
+    | _ -> None)
+  | New e -> (match typeInfer e with
+    | Some t -> Some t
+    | _ -> None)
+  | Deref e -> (match typeInfer e with
+    | Some TyRef t -> Some t
+    | _ -> None)
+  | Unit -> Some TyUnit
+  | Seq (e1, e2) -> (match typeInfer e1, typeInfer e2 with
+    | Some TyUnit, Some t -> Some t
+    | _ -> None)
+  | Read -> Some TyInt
+  | Print e -> (match typeInfer e with
+    | Some TyInt -> Some TyUnit
+    | _ -> None)
+
+
+
+
+
+(* casos de teste *)
+
           (*         
            
             let  x: int     =  read() in 
@@ -41,8 +91,6 @@ type expr =
             print (! y))     
 
 *)
-
-
 
 let cndwhi = Binop(Gt, Deref (Id "z"),Num 0)
 let asgny = Asg(Id "y", Binop(Mul, Deref (Id "y"),Deref(Id "z")))
